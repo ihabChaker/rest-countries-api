3a09662fcc00877f3842a903623a0420afeab049
ihabChaker <hi_benakcha@esi.dz>
2024-03-12 15:35:41 +0100

    completed country filter styling

--- src/components/countries_search_input/CountriesSearchInput.jsx
+++ src/components/countries_search_input/CountriesSearchInput.jsx
@@ -1,8 +1,14 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./countries-search-input.scss";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
 const CountriesSearchInput = () => {
   return (
     <div className="countries-search-input__container">
+      <FontAwesomeIcon
+        icon={faMagnifyingGlass}
+        className="fa-2x magnifying-loop-icon"
+      />
       <input type="text" name="" id="" placeholder="Search for a country..." />
     </div>
   );
